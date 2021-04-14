aca4ffc2a0bab7e1fc8d656a5f87323c9c7eba58
김지훈(Jihoon Kim)/App개발팀/11ST <jhkim077@sk.com>
2021-04-14 18:46:37 +0900

    zip 사용하여 livedata 2개 묶음

--- TDDToySample/app/src/main/java/com/duzi/tddtoysample/ui/single/SingleModeViewModel.kt
+++ TDDToySample/app/src/main/java/com/duzi/tddtoysample/ui/single/SingleModeViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.*
 import com.duzi.tddtoysample.domain.usecase.GenerateQuizUseCase
 import com.duzi.tddtoysample.result.process
 import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class SingleModeViewModel(
     private val generateQuizUseCase: GenerateQuizUseCase
@@ -24,11 +25,7 @@ class SingleModeViewModel(
     private val _guess = MutableLiveData<Int>()
 
     // 퀴즈 상태
-    private val _quizState = Transformations.switchMap(_guess) { guess ->
-        _answer.value?.let { answer ->
-            MutableLiveData(checkState(answer, guess))
-        }
-    }
+    private val _quizState = quizStateZipLiveData(_answer, _guess)
     val quizState: LiveData<QuizState> = _quizState
     private var tries: Int = 0
 
@@ -55,6 +52,31 @@ class SingleModeViewModel(
             guess < answer -> QuizState.DOWN
             else -> {
                 QuizState.BINGO(tries)
+            }
+        }
+    }
+
+    // https://medium.com/@gauravgyal/combine-results-from-multiple-async-requests-90b6b45978f7
+    private fun quizStateZipLiveData(answer: LiveData<Int>, guess: LiveData<Int>): LiveData<QuizState> {
+        return MediatorLiveData<QuizState>().apply {
+            var lastAnswer: Int? = null
+            var lastGuess: Int? = null
+
+            fun update() {
+                val localLastAnswer = lastAnswer
+                val localLastGuess = lastGuess
+                if (localLastAnswer != null && localLastGuess != null) {
+                    this.value = checkState(localLastAnswer, localLastGuess)
+                }
+            }
+
+            addSource(answer) {
+                lastAnswer = it
+                update()
+            }
+            addSource(guess) {
+                lastGuess = it
+                update()
             }
         }
     }
